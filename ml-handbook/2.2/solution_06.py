2e56ad6d043db643158e78c8385454e12ba699b2
Kirill Shmakov <shmakir@yandex.ru>
2026-03-13 21:53:16 +0100

    SGDLinearRegressor 4

--- ml-handbook/2.2/solution_06.py
+++ ml-handbook/2.2/solution_06.py
@@ -3,7 +3,7 @@ from sklearn.base import RegressorMixin
 
 class SGDLinearRegressor(RegressorMixin):
     def __init__(self,
-                 lr=0.01, regularization=1., delta_converged=1e-3, max_steps=1000,
+                 lr=0.01, regularization=1.0, delta_converged=1e-3, max_steps=1000,
                  batch_size=64):
         self.lr = lr
         self.regularization = regularization
@@ -18,34 +18,28 @@ class SGDLinearRegressor(RegressorMixin):
         self.W = np.random.uniform(-1.0, 1.0, size=X.shape[1])
         self.b = 0
 
-        self.steps = 0
+        for _ in range(self.max_steps):
+            ids = np.random.randint(0, X.shape[0], size=self.batch_size)
+            batch_n = len(ids)
 
-        for epoch in range(self.max_steps):
-            for start in range(0, X.shape[0], self.batch_size):
-                end = start + self.batch_size
+            X_batch = X[ids]
+            Y_batch = Y[ids]
 
-                X_batch = X[start:end]
-                Y_batch = Y[start:end]
+            lin = X_batch @ self.W + self.b - Y_batch
 
-                lin = X_batch @ self.W + self.b - Y_batch
+            dW = 2 * X_batch.T @ lin / batch_n + 2 * self.regularization * self.W
+            db = 2 * np.sum(lin) / batch_n
 
-                batch_n = X_batch.shape[0]
-                dW = 2 * X_batch.T @ lin / batch_n + 2 * self.regularization * self.W
-                db = 2 * np.sum(lin) / batch_n
+            dL2 = np.sqrt(np.sum(dW ** 2))
 
-                dL2 = np.sqrt(np.sum(dW ** 2))
+            dW = dW * self.lr
+            db = db * self.lr
 
-                dW = dW * self.lr
-                db = db * self.lr
+            self.W -= dW
+            self.b -= db
 
-                self.W -= dW
-                self.b -= db
-
-                self.steps += 1
-
-                if (self.steps > self.max_steps) or dL2 < self.delta_converged:
-                    return self
-
+            if dL2 < self.delta_converged:
+                break
 
         return self
 
