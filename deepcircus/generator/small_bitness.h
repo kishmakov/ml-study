83755261a85da63e2836a9aefbf3c841519c4c78
Kirill Shmakov <shmakir@yandex.ru>
2026-06-23 21:36:50 +0200

    Convert generator into stand-alone repository.

--- deepcircus/generator/small_bitness.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "decision_tree.h"
-
-inline constexpr uint16_t kExactTableBitness = 4;
-
-bool IsSmallBitness(uint16_t bitness);
-size_t SmallBitnessCasesNumber(uint16_t bitness);
-DecisionTree SmallBitnessTree(uint16_t bitness, size_t case_id);
